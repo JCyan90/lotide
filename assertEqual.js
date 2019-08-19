4158e8ee218820c07acb60fcfa3ae3cfcc308809
Julien Atanassian <julien.atanassian@gmail.com>
2019-08-19 21:51:47 +0000

    Used template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,6 +1,6 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-  return (actual === expected ? console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected) : console.log("❌❌❌ Assertion Failed: " + actual + " !== " + expected));
+  return (actual === expected ? console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`));
 };
 
 // TEST CODE
