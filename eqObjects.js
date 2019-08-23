063673b208251e281b1b926879a321ca893225b5
Julien Atanassian <julien.atanassian@gmail.com>
2019-08-23 20:56:47 +0000

    Changed eqObjects by switching to a forEach function

--- eqObjects.js
+++ eqObjects.js
@@ -18,28 +18,33 @@ const eqObjects = function(object1, object2) {
   const array2 = Object.keys(object2);
   // Compares objects length
   if (array1.length !== array2.length) return false;
-  for (let key of array1) {
+  array1.forEach(function(key) {
     // Uses the eqArrays function is values are arrays
     if (Array.isArray(object1[key]) || Array.isArray(object2[key])) {
       return (eqArrays(object1[key], object2[key]));
     // If they are not arrays assumes the values are primitives and compare them
     } else if (object1[key] !== object2[key]) return false;
-  }
+  })
   return true;
 };
 
 
 // Tests
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true);
+// const ab = { a: "1", b: "2" };
+// const ba = { b: "2", a: "1" };
+// assertEqual(eqObjects(ab, ba), true);
 
-const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false);
+// const abc = { a: "1", b: "2", c: "3" };
+// assertEqual(eqObjects(ab, abc), false);
 
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true);
+// const cd = { c: "1", d: ["2", 3] };
+// const dc = { d: ["2", 3], c: "1" };
+// assertEqual(eqObjects(cd, dc), true);
 
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false);
+// const cd2 = { c: "1", d: ["2", 3, 4] };
+// assertEqual(eqObjects(cd, cd2), false);
+
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true) // => true
+
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false) // => false
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false) // => false
