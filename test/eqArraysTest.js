fb4948bd35ccbda4dc91a6b5d0c4564a099ae559
Julien Atanassian <julien.atanassian@gmail.com>
2019-08-28 03:51:26 +0000

    Used recursion for eqArrays function

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -11,7 +11,16 @@ describe("#eqArrays", () => {
   it("should return true for (['1', '2', '3'], ['1', '2', '3'])", () => {
     assert.strictEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
   });
-  it("should return true for (['1', '2', '3'], ['1', '2', 3])", () => {
+  it("should return false for (['1', '2', '3'], ['1', '2', 3])", () => {
     assert.strictEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+  });
+  it("should return true for [[2, 3], [4]], [[2, 3], [4]])", () => {
+    assert.strictEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
+  });
+  it("should return false for ([[2, 3], [4]], [[2, 3], [4, 5])", () => {
+    assert.strictEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
+  });
+  it("should return false for ([[2, 3], [4]], [[2, 3], 4])", () => {
+    assert.strictEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
   });
 });
